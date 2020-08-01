eead2e822fc524d74e11fb2c4d6de754d4660ae3
vijay babar <vijbab.kalp@gmail.com>
2020-08-01 22:07:19 +0530

    Created dashboard controller in Admin.

--- MIDAMS/MIDAMS/Areas/Admin/AdminAreaRegistration.cs
+++ MIDAMS/MIDAMS/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,7 @@ namespace MIDAMS.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
